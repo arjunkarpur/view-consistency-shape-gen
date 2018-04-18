b4bbfd0f576424a96f64226e8da0dce713655c0a
Arjun Karpur <arjun.karpur@gmail.com>
2018-04-17 21:18:07 -0500

    Fix compare voxels script

--- src/voxel/compare_voxels.py
+++ src/voxel/compare_voxels.py
@@ -6,32 +6,33 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-pred_dir = "../../output/CHAIR/preds/test-ae3d/binary"
+pred_dir = "../../output/CHAIR/preds/chair-ae3d/binary"
 gt_dir = "../../data/CHAIR/mat/"
 
 for root, dirs, files in os.walk(pred_dir):
     random.shuffle(files)
     for f in files:
-        fp = os.path.join(dir_, f)
+        fp = os.path.join(pred_dir, f)
         mat = sio.loadmat(fp)
-        id_ = mat['id']
-        fig = plt.figure()
+        id_ = str(mat['id'][0])
+        fig = plt.figure(figsize=plt.figaspect(.5))
+        fig.suptitle(str(mat['id'][0]))
 
         # Get pred
-        fig.subplot(1,2,1)
         voxels = mat['data']
-        ax = fig.gca(projection='3d')
+        ax = fig.add_subplot(1, 2, 1, projection='3d')
         ax.voxels(voxels, edgecolors='k')
+        plt.title('Prediction')
 
         # Get GT
-        fig.subplot(1,2,2)
         fp = os.path.join(gt_dir, "%s.mat" % id_)
         mat = sio.loadmat(fp)
         gt_voxels = mat['data']
-        ax = fig.gca(projection='3d')
-        ax.voxels(gt_voxels, edgecolors='k')
+        ax = fig.add_subplot(1, 2, 2, projection='3d')
+        ax.voxels(gt_voxels, facecolors='red', edgecolors='k')
+        plt.title('Ground Truth')
 
         # Print to screen
-        print mat['id']
+        print str(mat['id'][0])
         plt.show()
     break
