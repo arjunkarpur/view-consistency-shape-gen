7da34e1411d32d9eac5d053fcbd5cb985e370a7c
Arjun Karpur <arjun.karpur@gmail.com>
2018-04-19 10:21:38 -0500

    Improve voxel viz tool

--- src/voxel/compare_voxels.py
+++ src/voxel/compare_voxels.py
@@ -6,7 +6,7 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-pred_dir = "../../output/CHAIR/preds/chair-ae3d/binary"
+pred_dir = "../../output/CHAIR/preds/chair-ae3d-long/binary"
 gt_dir = "../../data/CHAIR/mat/"
 
 for root, dirs, files in os.walk(pred_dir):
@@ -20,19 +20,29 @@ for root, dirs, files in os.walk(pred_dir):
 
         # Get pred
         voxels = mat['data']
-        ax = fig.add_subplot(1, 2, 1, projection='3d')
-        ax.voxels(voxels, edgecolors='k')
+        ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+        ax1.voxels(voxels, edgecolors='k')
         plt.title('Prediction')
 
         # Get GT
         fp = os.path.join(gt_dir, "%s.mat" % id_)
         mat = sio.loadmat(fp)
         gt_voxels = mat['data']
-        ax = fig.add_subplot(1, 2, 2, projection='3d')
-        ax.voxels(gt_voxels, facecolors='red', edgecolors='k')
+        ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+        ax2.voxels(gt_voxels, facecolors='red', edgecolors='k')
         plt.title('Ground Truth')
+
+        def on_move(event):
+            if event.inaxes == ax1:
+                ax2.view_init(elev=ax1.elev, azim=ax1.azim)
+            elif event.inaxes == ax2:
+                ax1.view_init(elev=ax2.elev, azim=ax2.azim)
+            else:
+                return
+            fig.canvas.draw_idle()
 
         # Print to screen
         print str(mat['id'][0])
+        c1 = fig.canvas.mpl_connect('motion_notify_event', on_move)
         plt.show()
     break
